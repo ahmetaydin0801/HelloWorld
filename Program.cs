f749e223bc4cf20e2ac910d4b6ca2fdac9e77958
Ahmet Aydın <ahmet.aydin@pubinno.com>
2024-09-14 19:08:40 +0300

    use dapper

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=True;Trusted_Connection=false;User Id=sa;Password=SQLConnect1!";
+            string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=True;Trusted_Connection=False;User Id=sa;Password=SQLConnect1";
 
             IDbConnection dbConnection = new SqlConnection(connectionString);
 
@@ -24,23 +24,39 @@ namespace HelloWorld
 
             Console.WriteLine($"The current date and time is: {rightNow}");
 
-            // Computer myComputer = new()
-            // {
-            //     Motherboard = "Asus",
-            //     CPUCores = 8,
-            //     HasWifi = true,
-            //     HasLTE = false,
-            //     ReleaseDate = DateTime.Now,
-            //     Price = 943.87m,
-            //     VideoCard = "Nvidia"
-            // };
+            Computer myComputer = new()
+            {
+                Motherboard = "Asus",
+                CPUCores = 8,
+                HasWifi = true,
+                HasLTE = false,
+                ReleaseDate = DateTime.Now,
+                Price = 943.87m,
+                VideoCard = "Nvidia"
+            };
 
-            // Console.WriteLine("My computer has the following specs:");
-            // Console.WriteLine($"Motherboard: {myComputer.Motherboard}");
-            // Console.WriteLine($"CPU Cores: {myComputer.CPUCores}");
-            // Console.WriteLine($"Has Wifi: {myComputer.HasWifi}");
-            // Console.WriteLine($"Has LTE: {myComputer.HasLTE}");
-            // Console.WriteLine($"Release Date: {myComputer.ReleaseDate}");
+            string sql = @"INSERT INTO TutorialAppSchema.Computer (
+                Motherboard
+                CPUCores
+                HasWifi
+                HasLTE,
+                ReleaseDate
+                Price
+                VideoCard
+            ) VALUES ('" + myComputer.Motherboard
+                    + "','" + myComputer.CPUCores
+                    + "','" + myComputer.HasWifi
+                    + "','" + myComputer.HasLTE
+                    + "','" + myComputer.ReleaseDate
+                    + "','" + myComputer.Price
+                    + "','" + myComputer.VideoCard
+            + "')";
+
+            Console.WriteLine(sql);
+
+            int result = dbConnection.Execute(sql);
+
+            Console.WriteLine(result);
         }
     }
 }
--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45f7f8d4bbe216a6f2842c7bc1d0ae8f8ff1ada3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80d3cdeb1995e4bc2667cc7210420d69693c13a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
