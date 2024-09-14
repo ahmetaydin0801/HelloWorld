80d3cdeb1995e4bc2667cc7210420d69693c13a5
Ahmet Aydın <ahmet.aydin@pubinno.com>
2024-09-14 18:20:04 +0300

    create connections

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using Dapper;
 using HelloWorld.Models;
+using Microsoft.Data.SqlClient;
 
 namespace HelloWorld
 {
@@ -11,23 +14,33 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            Computer myComputer = new()
-            {
-                Motherboard = "Asus",
-                CPUCores = 8,
-                HasWifi = true,
-                HasLTE = false,
-                ReleaseDate = DateTime.Now,
-                Price = 943.87m,
-                VideoCard = "Nvidia"
-            };
+            string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=True;Trusted_Connection=false;User Id=sa;Password=SQLConnect1!";
 
-            Console.WriteLine("My computer has the following specs:");
-            Console.WriteLine($"Motherboard: {myComputer.Motherboard}");
-            Console.WriteLine($"CPU Cores: {myComputer.CPUCores}");
-            Console.WriteLine($"Has Wifi: {myComputer.HasWifi}");
-            Console.WriteLine($"Has LTE: {myComputer.HasLTE}");
-            Console.WriteLine($"Release Date: {myComputer.ReleaseDate}");
+            IDbConnection dbConnection = new SqlConnection(connectionString);
+
+            string sqlCommand = "SELECT GETDATE()";
+
+            DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
+
+            Console.WriteLine($"The current date and time is: {rightNow}");
+
+            // Computer myComputer = new()
+            // {
+            //     Motherboard = "Asus",
+            //     CPUCores = 8,
+            //     HasWifi = true,
+            //     HasLTE = false,
+            //     ReleaseDate = DateTime.Now,
+            //     Price = 943.87m,
+            //     VideoCard = "Nvidia"
+            // };
+
+            // Console.WriteLine("My computer has the following specs:");
+            // Console.WriteLine($"Motherboard: {myComputer.Motherboard}");
+            // Console.WriteLine($"CPU Cores: {myComputer.CPUCores}");
+            // Console.WriteLine($"Has Wifi: {myComputer.HasWifi}");
+            // Console.WriteLine($"Has LTE: {myComputer.HasLTE}");
+            // Console.WriteLine($"Release Date: {myComputer.ReleaseDate}");
         }
     }
 }
--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023536c896c5d25b32a04b655419436ebd1b973d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45f7f8d4bbe216a6f2842c7bc1d0ae8f8ff1ada3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
